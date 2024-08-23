9b4f121611b169fdb21ab7febda829eeeaff5d4d
IldoRamos <ildoramosvieira@gmail.com>
2024-08-23 16:56:45 -0300

    criação de projeto

---
+++ src/main/java/com/ramos/pedido/PedidoComprasApplication.java
@@ -0,0 +1,13 @@
+package com.ramos.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramos/pedido/PedidoComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramos.pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
