36129d45e93e4bd5dfc0bd1ed98c56ec2cfc3350
IldoRamos <ildoramosvieira@gmail.com>
2024-08-26 20:53:13 -0300

    AJUSTE NAS CLASSES DE DOMINIO

--- src/main/java/com/ramos/pedido/domain/EnderecoRequest.java
+++ src/main/java/com/ramos/pedido/domain/EnderecoRequest.java
@@ -1,13 +1,39 @@
 package com.ramos.pedido.domain;
 
+import jakarta.persistence.Embeddable;
 import lombok.Data;
 import lombok.With;
 
-@Data
-@With
+@Embeddable
 public class EnderecoRequest {
 	private String rua;
     private String cidade;
     private String cep;
     private String pais;
+	public String getRua() {
+		return rua;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+    
+    
 }
--- src/main/java/com/ramos/pedido/domain/Produto.java
+++ src/main/java/com/ramos/pedido/domain/Produto.java
@@ -3,25 +3,24 @@ package com.ramos.pedido.domain;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ramos.pedido.domain.dto.ProdutoDTO;
 import com.ramos.pedido.domain.enums.Status;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import lombok.Builder;
-import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.With;
 
 
-@Data
-@With
-@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Produto implements Serializable{
@@ -41,10 +40,89 @@ public class Produto implements Serializable{
     
     private String metodoPagamento;
     
+    @Embedded
     private EnderecoRequest enderecoRequest;
   
     @JsonFormat(pattern = "dd/MM/yyyy")	
 	private LocalDate dataCompra = LocalDate.now();
     
     private Status status;
+    
+	public Produto(ProdutoDTO obj) {
+		super();
+		this.idProduto = obj.getIdProduto();
+		this.idCliente = obj.getIdCliente();
+		this.itensProduto = obj.getItensProduto().stream().map(x->x).collect(Collectors.toList());
+		this.valorTotal = obj.getValorTotal();
+		this.metodoPagamento = obj.getMetodoPagamento();
+		this.enderecoRequest = obj.getEnderecoRequest();
+		this.dataCompra = obj.getDataCompra();
+		this.status = obj.getStatus();
+	}
+
+	public Integer getIdProduto() {
+		return idProduto;
+	}
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public List<ItemProduto> getItensProduto() {
+		return itensProduto;
+	}
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+	public String getMetodoPagamento() {
+		return metodoPagamento;
+	}
+
+	public EnderecoRequest getEnderecoRequest() {
+		return enderecoRequest;
+	}
+
+	public LocalDate getDataCompra() {
+		return dataCompra;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setIdProduto(Integer idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public void setItensProduto(List<ItemProduto> itensProduto) {
+		this.itensProduto = itensProduto.stream().map(x->x).collect(Collectors.toList());
+	}
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public void setMetodoPagamento(String metodoPagamento) {
+		this.metodoPagamento = metodoPagamento;
+	}
+
+	public void setEnderecoRequest(EnderecoRequest enderecoRequest) {
+		this.enderecoRequest = enderecoRequest;
+	}
+
+	public void setDataCompra(LocalDate dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+    
+    
 }
